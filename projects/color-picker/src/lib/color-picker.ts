c84360427759ca8166d827e18ae3b5a25d492499
nzbin <nzb329@163.com>
2025-12-12 20:57:44 +0800

    chore: init project

---
+++ projects/color-picker/src/lib/color-picker.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-color-picker',
+  imports: [],
+  template: `
+    <p>color-picker works!</p>
+  `,
+  styles: ``,
+})
+export class ColorPicker {}
