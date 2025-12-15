8d39acccd0712aaf509ab0c73a059dcbff2e7fde
nzbin <nzb329@163.com>
2025-12-15 23:41:38 +0800

    refactor: add css vars and support dark theme

--- projects/dev-app/src/app/app.component.ts
+++ projects/dev-app/src/app/app.component.ts
@@ -9,6 +9,8 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  theme: 'light' | 'dark' = 'light';
+
   color = '#ff000060';
   color2 = 'rgba(200, 100, 100, 1)';
   color3 = 'hsla(200, 100%, 50%, 1)';
@@ -16,6 +18,14 @@ export class AppComponent {
   format: ColorFormat = 'hex';
 
   disableAlpha = false;
+
+  onThemeChange() {
+    if (this.theme === 'light') {
+      document.querySelector('html')!.classList.remove('theme-dark');
+    } else if (this.theme === 'dark') {
+      document.querySelector('html')!.classList.add('theme-dark');
+    }
+  }
 
   log(type: string, e: any) {
     console.log(type, e);
