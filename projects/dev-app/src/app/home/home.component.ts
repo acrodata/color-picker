4cf6ca2a0708d6f6411ba60410ea7ff849430dfe
nzbin <nzb329@163.com>
2025-12-19 23:48:20 +0800

    refactor(dev-app): improve the home styles

--- projects/dev-app/src/app/home/home.component.ts
+++ projects/dev-app/src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { ColorPicker } from '@acrodata/color-picker';
+import { CdkConnectedOverlay, CdkOverlayOrigin } from '@angular/cdk/overlay';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [ColorPicker],
+  imports: [FormsModule, ColorPicker, CdkConnectedOverlay, CdkOverlayOrigin],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   color = '#ff0000';
+
+  isOpen = false;
 }
